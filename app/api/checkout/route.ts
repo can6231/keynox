7f5e94078216efc99d21f51c273403555852c543
Can <cemilcan20@icloud.com>
2026-04-28 02:41:02 +0300

    Update route.ts

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2023-10-16",
+}) as string);
 
 export async function GET() {
   const session = await stripe.checkout.sessions.create({
