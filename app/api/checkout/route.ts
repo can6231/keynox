f8d7671c0428219b58bf094e1bb9a19f8658c95e
Can <cemilcan20@icloud.com>
2026-04-28 01:49:02 +0300

    Create route.ts

---
+++ app/api/checkout/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+export async function GET() {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "try",
+          product_data: {
+            name: "Cyber Quest",
+          },
+          unit_amount: 29900,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: "https://keynox-kappa.vercel.app/success",
+    cancel_url: "https://keynox-kappa.vercel.app/cart",
+  });
+
+  return NextResponse.redirect(session.url!);
+}
