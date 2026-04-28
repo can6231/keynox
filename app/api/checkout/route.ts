fd2f06fadedde495203570c44368ba1689000577
Can <cemilcan20@icloud.com>
2026-04-28 15:48:47 +0300

    Update route.ts

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,29 +1,46 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
+import { getDb } from "../../../lib/mongodb";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2023-10-16" as any,
 });
 
-export async function POST(req: Request) {
-  const { cart } = await req.json();
+export async function GET() {
+  const db = await getDb();
 
+  // 🔥 stok kontrol
+  const stock = await db.collection("keys").countDocuments({
+    used: false,
+  });
+
+  if (stock === 0) {
+    return NextResponse.json(
+      { error: "Stok tükendi" },
+      { status: 400 }
+    );
+  }
+
+  // 💳 Stripe ödeme oluştur
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
-    mode: "payment",
-    line_items: cart.map((item: any) => ({
-      quantity: 1,
-      price_data: {
-        currency: "try",
-        unit_amount: Number(item.price.replace(" TL", "")) * 100,
-        product_data: {
-          name: `${item.name} - ${item.platform}`,
+    line_items: [
+      {
+        price_data: {
+          currency: "try",
+          product_data: {
+            name: "Cyber Quest",
+          },
+          unit_amount: 29900,
         },
+        quantity: 1,
       },
-    })),
+    ],
+    mode: "payment",
+
     success_url: "https://keynox-kappa.vercel.app/success",
     cancel_url: "https://keynox-kappa.vercel.app/cart",
   });
 
-  return NextResponse.json({ url: session.url });
+  return NextResponse.redirect(session.url!);
 }
