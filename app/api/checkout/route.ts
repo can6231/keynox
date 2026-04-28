cfff7b565e584d1bab4f4c8840e517ddda795a9d
Can <cemilcan20@icloud.com>
2026-04-28 15:55:57 +0300

    Update route.ts

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -6,7 +6,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2023-10-16" as any,
 });
 
-export async function GET() {
+export async function POST(req: Request) {
+  const { cart } = await req.json();
   const db = await getDb();
 
   // 🔥 stok kontrol
@@ -24,23 +25,20 @@ export async function GET() {
   // 💳 Stripe ödeme oluştur
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
-    line_items: [
-      {
-        price_data: {
-          currency: "try",
-          product_data: {
-            name: "Cyber Quest",
-          },
-          unit_amount: 29900,
-        },
-        quantity: 1,
-      },
-    ],
     mode: "payment",
-
+    line_items: cart.map((item: any) => ({
+      quantity: 1,
+      price_data: {
+        currency: "try",
+        unit_amount: Number(item.price.replace(" TL", "")) * 100,
+        product_data: {
+          name: `${item.name} - ${item.platform}`,
+        },
+      },
+    })),
     success_url: "https://keynox-kappa.vercel.app/success",
     cancel_url: "https://keynox-kappa.vercel.app/cart",
   });
 
-  return NextResponse.redirect(session.url!);
+  return NextResponse.json({ url: session.url });
 }
