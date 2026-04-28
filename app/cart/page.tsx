cb56a3fd8b45f9d34f564438ce724fe298724ba6
Can <cemilcan20@icloud.com>
2026-04-28 03:12:17 +0300

    Update page.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -26,6 +26,9 @@ export default function CartPage() {
         > 
           Ödemeye Geç
         </a>
+        <p style={{ color: "red" }}>
+          Ödeme iptal edildi
+        </p>
       </div>
     </main>
   );
